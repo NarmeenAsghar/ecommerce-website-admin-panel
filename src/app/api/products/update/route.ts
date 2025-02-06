398d06433cd49f4e238e4ffa7544c55ee3fafd7f
NarmeenAsghar <163905451+NarmeenAsghar@users.noreply.github.com>
2025-02-06 15:15:13 +0500

    Update route.ts

--- src/app/api/products/update/route.ts
+++ src/app/api/products/update/route.ts
@@ -1,40 +1,40 @@
-// pages/api/products/update.ts
+// // pages/api/products/update.ts
 
-import { NextApiRequest, NextApiResponse } from 'next';
-import { client } from '@/sanity/lib/client';
+// import { NextApiRequest, NextApiResponse } from 'next';
+// import { client } from '@/sanity/lib/client';
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === 'POST') {
-    try {
-      const productData = req.body; // Extract product data from the request body
+// const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+//   if (req.method === 'POST') {
+//     try {
+//       const productData = req.body; // Extract product data from the request body
 
-      const { name, details, price, priceWithoutDiscount, category, image } = productData;
+//       const { name, details, price, priceWithoutDiscount, category, image } = productData;
 
-      // Create a new product document in Sanity
-      const product = await client.create({
-        _type: 'product',
-        name,
-        details,
-        price,
-        priceWithoutDiscount,
-        category,
-        image: {
-          _type: 'image',
-          asset: {
-            _type: 'reference',
-            _ref: image, // The reference to the uploaded image
-          },
-        },
-      });
+//       // Create a new product document in Sanity
+//       const product = await client.create({
+//         _type: 'product',
+//         name,
+//         details,
+//         price,
+//         priceWithoutDiscount,
+//         category,
+//         image: {
+//           _type: 'image',
+//           asset: {
+//             _type: 'reference',
+//             _ref: image, // The reference to the uploaded image
+//           },
+//         },
+//       });
 
-      res.status(200).json({ message: 'Product added successfully', product });
-    } catch (error) {
-      console.error('Error adding product:', error);
-      res.status(500).json({ message: 'Failed to add product' });
-    }
-  } else {
-    res.status(405).json({ message: 'Method Not Allowed' });
-  }
-};
+//       res.status(200).json({ message: 'Product added successfully', product });
+//     } catch (error) {
+//       console.error('Error adding product:', error);
+//       res.status(500).json({ message: 'Failed to add product' });
+//     }
+//   } else {
+//     res.status(405).json({ message: 'Method Not Allowed' });
+//   }
+// };
 
-export default handler;
+// export default handler;
